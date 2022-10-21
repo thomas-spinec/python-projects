f66869eae2f5a838c2b2024831a13062a5797a9b
thomas-spinec <thomas.spinec@laplateforme.io>
2022-10-21 21:09:35 +0200

    rajout des fonctions à la place de la boucle du jeu

--- pendu.py
+++ pendu.py
@@ -4,31 +4,61 @@ from random import randrange
 ################# Définition des fonctions #################
 #fonction pour mettre en minuscule et enlever les accents
 def case(phrase):
-    alphabet = 'abcdefghijklmnopqrstuvwxyzaaeeeeiiABCDEFGHIJKLMNOPQRSTUVWXYZàâéèêëîï'
+    alphabet = 'abcdefghijklmnopqrstuvwxyzaaeeeeiioucABCDEFGHIJKLMNOPQRSTUVWXYZàâéèêëîïôùç'
     result = ""
     for x in phrase:
-        if x not in alphabet or alphabet.index(x)<=34:  #cherche dans l'alphabet si la lettre est en minuscule
+        if x not in alphabet or alphabet.index(x)<=35:  #cherche dans l'alphabet si la lettre est en minuscule
             result += x
-        elif x in alphabet and alphabet.index(x)>34:   #cherche dans l'alphabet si la lettre est en majuscule
-            result += alphabet[alphabet.index(x)-34]
+        elif x in alphabet and alphabet.index(x)>35:   #cherche dans l'alphabet si la lettre est en majuscule
+            result += alphabet[alphabet.index(x)-35]
     return result
 
-#####     définition de la fonction de vérification de la lettre dans le mot #####
-def pendu(lettre):
-    global mot, soluce, nb_essais
-    #boucle de vérification de la lettre dans le mot
-    if lettre in soluce:
-        print("-------------------------")
-        print("\nLa lettre", lettre, "est dans le mot.")
-        #boucle de remplacement des "-" par la lettre
-        for i in range(len(soluce)):
-            if soluce[i] == lettre:
-                mot = mot[:i] + lettre + mot[i+1:]
-    else:
-        print("-------------------------")
-        print("\nLa lettre", lettre, "n'est pas dans le mot.")
-        nb_essais -= 1
-    return mot, nb_essais
+#####  fonction du jeu #####
+#boucle du jeu
+def pendu(level, nb_essais, mot, lettreutilisee):
+    global soluce
+    print("\n--------------------")
+    print("Vous avez choisi la difficulté", level, ". Vous avez", nb_essais, "essais.")
+    if level == "expert":
+        print("dans cette difficulté, les lettres déjà utilisées ne sont pas affichées.")
+    elif level == "extrême":
+        print("Cependant, les lettres déjà utilisées ne sont pas affichées.")
+        print("De plus, le mot est caché, sauf les lettres trouvées.")
+    while nb_essais != 0 and mot != soluce:
+        if diff < 4:
+            print ("le mot contient ", len(soluce), "lettres")
+        print (mot)
+        lettre = input("Choisissez une lettre : ")
+
+        #boucle de vérification d'erreur de saisie
+        while len(lettre) != 1 or lettre.isalpha() == False:
+            print("Veuillez saisir une lettre et seulement une (pas un chiffre).")
+            lettre = input("Choisissez une lettre : ")
+        lettreutilisee = lettreutilisee + lettre + ", "
+
+        #boucle de vérification de la lettre dans le mot
+        if lettre in soluce:
+            print("-------------------------")
+            print("\nLa lettre -", lettre, "- est dans le mot.")
+            #boucle de remplacement des "-" par la lettre
+            for i in range(len(soluce)):
+                if soluce[i] == lettre:
+                    mot = mot[:i] + lettre + mot[i+1:]
+        else:
+            print("-------------------------")
+            print("\nLa lettre", lettre, "n'est pas dans le mot.")
+            nb_essais -= 1
+
+        print ("Il vous reste", nb_essais, "essais.")
+        if diff < 3:
+            print ("lettres déjà utilisées :", lettreutilisee)
+    #boucle de vérification de victoire
+    if mot == soluce:
+        victoire(lettreutilisee, soluce, nb_essais)
+    
+    elif nb_essais == 0:
+        defaite(lettreutilisee,soluce)
+    # return mot, nb_essais, lettreutilisee
 
 #####      fonction de victoire
 def victoire(lettreutilisee, soluce, nb_essais):
@@ -81,7 +111,8 @@ while choix == "o":
         print("En mode facile, vous avez le droit à 10 erreurs pour trouver le mot mystère.")
         print("En mode intérmédiaire, vous avez le droit à 7 erreurs.")
         print("En mode expert, vous avez le droit à 5 erreurs\nDe plus les lettres déjà utilisées ne sont pas affichées dans ce mode.")
-        print("En mode extrême, vous avez le droit à 10 erreurs mais les lettres déjà utilisées ne sont pas affichées, et seules les lettres correctes sont affichées")
+        print("En mode extrême, vous avez le droit à 10 erreurs mais les lettres déjà utilisées ne sont pas affichées")
+        print("De plus le mot est caché et seules les lettres correctes sont affichées")
         inf = input("\nTapez 'd' pour aller au choix des difficultées :")
         while inf != "d":
             inf = input("\nVeuillez taper 'd' pour aller au choix des difficultées : ")
@@ -100,122 +131,34 @@ while choix == "o":
     if diff == 1 :
         nb_essais = 10
         lettreutilisee = ""
-        print("\n--------------------")
-        print("Vous avez choisi la difficulté facile. Vous avez", nb_essais, "essais.")
+        level = "facile"
         #boucle du jeu
-        while nb_essais != 0 and mot != soluce:
-            print ("le mot contient ", len(soluce), "lettres")
-            print (mot)
-            lettre = input("Choisissez une lettre : ")
-
-            #boucle de vérification d'erreur de saisie
-            while len(lettre) != 1 or lettre.isalpha() == False:
-                print("Veuillez saisir une lettre et seulement une (pas un chiffre).")
-                lettre = input("Choisissez une lettre : ")
-            lettreutilisee = lettreutilisee + lettre + ", "
-
-            ###### fonction pendu ######
-            pendu(lettre)
-            ############################
-
-            print ("Il vous reste", nb_essais, "essais.")
-            print ("lettres déjà utilisées :", lettreutilisee)
-
-        #boucle de vérification de victoire
-        if mot == soluce:
-            victoire(lettreutilisee, soluce, nb_essais)
-        
-        elif nb_essais == 0:
-            defaite(lettreutilisee,soluce)
+        pendu(level, nb_essais, mot, lettreutilisee)
 
     #Difficulté moyenne
     elif diff == 2:
         nb_essais = 7
         lettreutilisee = ""
-        print("\n--------------------")
-        print("Vous avez choisi la difficulté intermédiaire. Vous avez", nb_essais, "essais.")
+        level = "intermédiaire"
         #boucle du jeu
-        while nb_essais != 0 and mot != soluce:
-            print ("le mot contient ", len(soluce), "lettres")
-            print (mot)
-            lettre = input("Choisissez une lettre : ")
-            #boucle de vérification d'erreur de saisie
-            while len(lettre) != 1 or lettre.isalpha() == False:
-                print("Veuillez saisir une lettre et seulement une (pas un chiffre).")
-                lettre = input("Choisissez une lettre : ")
-            lettreutilisee = lettreutilisee + lettre + ", "
-            ###### fonction pendu ######
-            pendu(lettre)
-            ############################
-            print ("Il vous reste", nb_essais, "essais.")
-            print ("lettres déjà utilisées :", lettreutilisee)
-
-        #boucle de vérification de victoire
-        if mot == soluce:
-            victoire(lettreutilisee, soluce, nb_essais)
-
-        elif nb_essais == 0:
-            defaite(lettreutilisee,soluce)
+        pendu(level, nb_essais, mot, lettreutilisee)
 
     #Difficulté expert
     elif diff == 3:
         nb_essais = 5
         lettreutilisee = ""
-        print("\n--------------------")
-        print("Vous avez choisi la difficulté expert. Vous avez", nb_essais, "essais.")
-        print("dans cette difficulté, les lettres déjà utilisées ne sont pas affichées.")
+        level = "expert"
         #boucle du jeu
-        while nb_essais != 0 and mot != soluce:
-            print ("le mot contient ", len(soluce), "lettres")
-            print (mot)
-            lettre = input("Choisissez une lettre : ")
-            #boucle de vérification d'erreur de saisie
-            while len(lettre) != 1 or lettre.isalpha() == False:
-                print("Veuillez saisir une lettre et seulement une (pas un chiffre).")
-                lettre = input("Choisissez une lettre : ")
-            lettreutilisee = lettreutilisee + lettre + ", "
-            ###### fonction pendu ######
-            pendu(lettre)
-            ############################
-            print ("Il vous reste", nb_essais, "essais.")
-
-        #boucle de vérification de victoire
-        if mot == soluce:
-            victoire(lettreutilisee, soluce, nb_essais)
-
-        elif nb_essais == 0:
-            defaite(lettreutilisee,soluce)
+        pendu(level, nb_essais, mot, lettreutilisee)
 
     #Difficulté extrême
     elif diff == 4:
         mot = " " * (len(soluce))
         nb_essais = 10
         lettreutilisee = ""
-        print("\n--------------------")
-        print("Vous avez choisi la difficulté extrême. Vous avez", nb_essais, "essais.")
-        print("Cependant, les lettres déjà utilisées ne sont pas affichées.")
-        print("De plus, le mot est caché, sauf les lettres trouvées.")
+        level = "extrême"
         #boucle du jeu
-        while nb_essais != 0 and mot != soluce:
-            print (mot)
-            lettre = input("Choisissez une lettre : ")
-            #boucle de vérification d'erreur de saisie
-            while len(lettre) != 1 or lettre.isalpha() == False:
-                print("Veuillez saisir une lettre et seulement une (pas un chiffre).")
-                lettre = input("Choisissez une lettre : ")
-            lettreutilisee = lettreutilisee + lettre + ", "
-            ###### fonction pendu ######
-            pendu(lettre)
-            ############################
-            print ("Il vous reste", nb_essais, "essais.")
-
-        #boucle de vérification de victoire
-        if mot == soluce:
-            victoire(lettreutilisee, soluce, nb_essais)
-
-        elif nb_essais == 0:
-            defaite(lettreutilisee,soluce)
-
+        pendu(level, nb_essais, mot, lettreutilisee)
 
     ###        Vouloir rejouer ?        ###
     print("\n--------------------")
